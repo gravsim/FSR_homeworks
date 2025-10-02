fe5065c5568b26f3f2495b0ffd7193c57958e5d1
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-10-02 23:25:22 +0300

    B COMPLETE MOTHERFUCKER

--- informatics/multydimensional/B.c
+++ informatics/multydimensional/B.c
@@ -2,47 +2,68 @@
 #include <stdlib.h>
 
 
-int main() {
-    long W;
-    long H;
-    long N;
-    long dots = 0;
-    scanf("%ld %ld %ld", &W, &H, &N);
-    long centers[1000000][2];
-    long i, j;
-    long x, y;
-    long unique = 0;
-    for (i = 0; i < N; i++) {
-        scanf("%ld %ld", &x, &y);
-        if (x > 0 && x <= W && y > 0 && y <= H) {
-            dots++;
-        }
-        centers[i][0] = x;
-        centers[i][1] = y;
-    }
-    int all_colored = 1;
+int compare(const void *a, const void *b) {
+    const long long *p_a = a;
+    const long long *p_b = b;
+    if (p_a[0] != p_b[0]) return (p_a[0] < p_b[0]) ? -1 : 1;
+    if (p_a[1] != p_b[1]) return (p_a[1] < p_b[1]) ? -1 : 1;
+    return 0;
+}
 
-    if (dots < W * H / 4) {
+
+int on_canvas(long long x, long long y, long long W, long long H) {
+    if (x > 0 && x <= W && y > 0 && y <= H) {
+        return 1;
+    }
+    return 0;
+}
+
+
+void check(long long x, long long y, long long W, long long H, long long colored[][2], long long *k) {
+    if (on_canvas(x, y, W, H)) {
+        colored[*k][0] = x;
+        colored[*k][1] = y;
+        (*k)++;
+    }
+}
+
+
+int main() {
+    long long W;
+    long long H;
+    long long N;
+    scanf("%lld %lld %lld", &W, &H, &N);
+    if (N < W * H / 5) {
         printf("No");
         return 0;
     }
+
+    long long(*colored)[2] = malloc(sizeof(long long[5 * N][2]));
+    long long i, j, k;
+    long long x, y;
+    k = 0;
     for (i = 0; i < N; i++) {
-        if (centers[i][0] != -1) {
-            unique++;
-            for (j = i + 1; j < N; j++) {
-                if (centers[i][0] == centers[j][0]
-                    && centers[i][1] == centers[j][1]
-                    && centers[i][0] != -1) {
-                    centers[j][0] = -1;
-                }
-            }
-        }
+        scanf("%lld %lld", &x, &y);
+        check(x, y, W, H, colored, &k);
+        check(x + 1, y, W, H, colored, &k);
+        check(x - 1, y, W, H, colored, &k);
+        check(x, y + 1, W, H, colored, &k);
+        check(x, y - 1, W, H, colored, &k);
     }
 
-    if (all_colored) {
+    qsort(colored, k, sizeof(long long[2]), compare);
+    long long unique = 0;
+    for (long long i = 0; i < k; i++) {
+        if (i == 0 || colored[i][0] != colored[i - 1][0] || colored[i][1] != colored[i - 1][1]) {
+            unique++;
+        }  
+    }
+
+    if (unique >= H * W) {
         printf("Yes");
     } else {
         printf("No");
     }
+    free(colored);
     return 0;
 }
