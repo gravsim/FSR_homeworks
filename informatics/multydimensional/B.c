7cc9f0525ab78c7dc7aace5e7047fed1ff9215a8
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-10-23 20:45:08 +0300

    too slow

--- informatics/multydimensional/B.c
+++ informatics/multydimensional/B.c
@@ -2,12 +2,44 @@
 #include <stdlib.h>
 
 
-int compare(const void *a, const void *b) {
-    const long long *p_a = a;
-    const long long *p_b = b;
-    if (p_a[0] != p_b[0]) return (p_a[0] < p_b[0]) ? -1 : 1;
-    if (p_a[1] != p_b[1]) return (p_a[1] < p_b[1]) ? -1 : 1;
+int is_smaller(long long a[2], long long b[2]) {
+    if (a[0] != b[0]) return a[0] < b[0];
+    if (a[1] != b[1]) return a[1] < b[1];
     return 0;
+}
+
+
+void swap(long long a[2], long long b[2]) {
+    long long tmp = a[0];
+    a[0] = b[0];
+    b[0] = tmp;
+
+    tmp = a[1];
+    a[1] = b[1];
+    b[1] = tmp;
+}
+
+
+long long partition(long long array[][2], long long start, long long end) {
+    long long i;
+    long long swap_index = start;
+    for (i = start; i < end; i++) {
+        if (is_smaller(array[i], array[end])) {
+            swap(array[i], array[swap_index]);
+            swap_index++;
+        }
+    }
+    swap(array[end], array[swap_index]);
+    return swap_index;
+}
+
+
+void quick_sort(long long array[][2], long long start, long long end) {
+    if (start < end) {
+        long long partition_index = partition(array, start, end);
+        quick_sort(array, start, partition_index - 1);
+        quick_sort(array, partition_index + 1, end);
+    }
 }
 
 
@@ -51,7 +83,7 @@ int main() {
         check(x, y - 1, W, H, colored, &k);
     }
 
-    qsort(colored, k, sizeof(long long[2]), compare);
+    quick_sort(colored, 0, k - 1);
     long long unique = 0;
     for (long long i = 0; i < k; i++) {
         if (i == 0 || colored[i][0] != colored[i - 1][0] || colored[i][1] != colored[i - 1][1]) {
