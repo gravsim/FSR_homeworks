b52a1c676d6378a6458cfa88e79a5906600a878f
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-11-03 15:25:52 +0300

    а че у тебя функции то, именные? не в масть тебе такие функции, давай снимай

--- informatics/pointers/A.c
+++ informatics/pointers/A.c
@@ -1,26 +1,59 @@
 #include <stdio.h>
 
 
-int main() {
-    int N;
-    scanf("%d", &N);
-    int A, B, C;
-    int iteration;
-    int amount = 0;
-    if (N < 3) {
-        printf("0");
-        return 0;
-    }
-    scanf("%d", &A);
-    scanf("%d", &B);
-    for (iteration = 0; iteration < N - 2; iteration++) {
-        scanf("%d", &C);
-        if (A < B && B > C) {
-            amount++;
+#define ilya_klopov_zero 0
+#define ilya_klopov_two 2
+#define ilya_klopov_three 3
+typedef int ilya_klopov_int;
+typedef void ilya_klopov_void;
+
+
+ilya_klopov_void ilya_klopov_scanf(ilya_klopov_int* variable) {
+    scanf("%d", &variable);
+}
+
+
+ilya_klopov_void ilya_klopov_printf(ilya_klopov_int* variable) {
+    printf("%d", &variable);
+}
+
+
+ilya_klopov_void ilya_klopov_increment(ilya_klopov_int* variable) {
+    (*variable)++;
+}
+
+
+ilya_klopov_void ilya_klopov_cicle(
+    ilya_klopov_int* iteration,
+    ilya_klopov_int* N,
+    ilya_klopov_int* A,
+    ilya_klopov_int* B,
+    ilya_klopov_int* C,
+    ilya_klopov_int* amount) {
+    for (*iteration = ilya_klopov_zero; *iteration < *N - ilya_klopov_two; ilya_klopov_increment(iteration)) {
+        ilya_klopov_scanf(&C);
+        if (*A < *B && *B > *C) {
+            ilya_klopov_increment(amount);
         }
-        A = B;
-        B = C;
+        *A = *B;
+        *B = *C;
     }
-    printf("%d", amount);
-    return 0;
+}
+
+
+ilya_klopov_int main(ilya_klopov_void) {
+    ilya_klopov_int N;
+    ilya_klopov_scanf(&N);
+    ilya_klopov_int A, B, C;
+    ilya_klopov_int iteration;
+    ilya_klopov_int amount = ilya_klopov_zero;
+    if (N < ilya_klopov_three) {
+        ilya_klopov_printf(ilya_klopov_zero);
+        return ilya_klopov_zero;
+    }
+    ilya_klopov_scanf(&A);
+    ilya_klopov_scanf(&B);
+    ilya_klopov_cicle(&iteration, &N, &A, &B, &C, &amount);
+    ilya_klopov_printf(amount);
+    return ilya_klopov_zero;
 }
