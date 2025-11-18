c99d7dc70b2f70bc9d4317e3e11c88c28299559e
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-11-19 00:00:06 +0300

    B done

--- yandex/23_10_2025/B.c
+++ yandex/23_10_2025/B.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct Node
 {
     int value;
@@ -9,28 +10,31 @@ typedef struct Node
 } Node;
 
 
-int insert(Node** beg_pp, int value) {
+int insert(Node** beg_pp, Node** end_pp, int value) {
     Node* new_node = malloc(sizeof(Node));
     if (!new_node) {
         return -2;
     }
     new_node->value = value;
     if (!*beg_pp) {
-        new_node->next = NULL;
-    } else {
-        (*beg_pp)->previous = new_node;
-        new_node->next = *beg_pp;
+        *beg_pp = new_node;
     }
-    new_node->previous = NULL;
-    *beg_pp = new_node;
+    if (*end_pp) {
+        (*end_pp)->next = new_node;
+        new_node->previous = *end_pp;
+    } else {
+        new_node->previous = NULL;
+    }
+    new_node->next = NULL;
+    *end_pp = new_node;
     return 0;
 }
 
 
 int print_list(Node* element_p) {
     if (element_p) {
-        print_list(element_p->next);
         printf("%d ", element_p->value);
+        print_list(element_p->next);
     }
     return 0;
 }
@@ -49,30 +53,31 @@ int search_element(Node** beg_pp, int target, Node** result) {
 }
 
 
-int put_in_beginning(Node** beg_pp, Node** node) {
-    if (!(*node)->previous) {
+int put_in_end(Node** beg_pp, Node** end_pp, Node** node) {
+    if (!(*node)->next) {
         return 1;
     }
-    if (!(*node)->next) {
-        (*node)->previous->next = NULL;
-    } else {
+    if ((*node)->previous) {
         (*node)->next->previous = (*node)->previous;
         (*node)->previous->next = (*node)->next;
+    } else {
+        (*node)->next->previous = NULL;
+        *beg_pp = (*node)->next;
     }
-    (*node)->previous = NULL;
-    (*node)->next = *beg_pp;
-    (*beg_pp)->previous = *node;
-    *beg_pp = *node;
+    (*node)->next = NULL;
+    (*node)->previous = *end_pp;
+    (*end_pp)->next = *node;
+    *end_pp = *node;
     return 0;
 }
 
 
-int read_list(int list_len, Node** beg_pp) {
+int read_list(int list_len, Node** beg_pp, Node** end_pp) {
     int i;
     int number;
     for (i = 0; i < list_len; i++) {
         scanf("%d", &number);
-        insert(beg_pp, number);
+        insert(beg_pp, end_pp, number);
     }
     return 0;
 }
@@ -96,10 +101,11 @@ int main(void) {
     Node* result;
     scanf("%d", &len);
     Node* beg_p = NULL;
-    read_list(len, &beg_p);
+    Node* end_p = NULL;
+    read_list(len, &beg_p, &end_p);
     scanf("%d", &target);
     while (search_element(&beg_p, target, &result)) {
-        put_in_beginning(&beg_p, &result);
+        put_in_end(&beg_p, &end_p, &result);
         scanf("%d", &target);
     }
     print_list(beg_p);
