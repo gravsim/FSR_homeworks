f642e54ab67300cf8fbe173ee23e424acf3ee540
Ilya Klopov <ilyaklopov07@yandex.ru>
2025-12-14 19:44:00 +0300

    apple govno android luchshe

--- informatics/autumn_graphs/E.c
+++ informatics/autumn_graphs/E.c
@@ -3,55 +3,10 @@
 #include <math.h>
 
 
-int is_smaller(int* a, int* b) {
-    return a[2] < b[2];
+int touches(int* upper, int* lower) {
+    double distance = sqrt(pow(upper[0]  - lower[0], 2) + pow(upper[1] - lower[1], 2));
+    return upper[2] >= lower[2] && distance <= upper[3] + lower[3];
 }
-
-
-void swap(int* a, int* b) {
-    int tmp = a[0];
-    a[0] = b[0];
-    b[0] = tmp;
-
-    tmp = a[1];
-    a[1] = b[1];
-    b[1] = tmp;
-
-    tmp = a[2];
-    a[2] = b[2];
-    b[2] = tmp;
-
-    tmp = a[3];
-    a[3] = b[3];
-    b[3] = tmp;
-}
-
-
-void quick_sort(int** array, int size){
-    int down = 0;
-    int up = size - 1;
-    int pivot[3] = {array[size / 2][0], array[size / 2][1], array[size / 2][2]};
-    if (size > 1){
-        while (down <= up){
-            while (is_smaller(array[down], pivot)) down++;
-            while (is_smaller(pivot, array[up])) up--;
-            if (down <= up){
-                swap(array[down], array[up]);
-                down++;
-                up--;
-            }
-        }
-        quick_sort(array, up + 1);
-        quick_sort(array + down, size - down);
-    }
-}
-
-
-int touches(int* a, int* b) {
-    double distance = sqrt(pow(a[0]  - b[0], 2) + pow(a[1] - b[1], 2));
-    return distance < a[3] + b[3];
-}
-
 
 
 int fall(int** apples, int* falling, int* falls_amount, int current, int N) {
@@ -63,6 +18,7 @@ int fall(int** apples, int* falling, int* falls_amount, int current, int N) {
         if (!falling[i] && touches(apples[current], apples[i])) {
             falling[i] = 1;
             (*falls_amount)++;
+            fall(apples, falling, falls_amount, i, N);
         }
     }
     return 1;
@@ -73,8 +29,9 @@ int main(void) {
     int N;
     scanf("%d", &N);
     int** apples = calloc(N, sizeof(int*));
-    int* falling = calloc(N, sizeof(int*));
-    int falls_amount = 0;
+    int* falling = calloc(N, sizeof(int));
+    int falls_amount = 1;
+    falling[0] = 1;
     int i;
     for (i = 0; i < N; i++) {
         apples[i] = calloc(4, sizeof(int));
@@ -82,7 +39,6 @@ int main(void) {
     for (i = 0; i < N; i++) {
         scanf("%d %d %d %d", &apples[i][0], &apples[i][1], &apples[i][2], &apples[i][3]);
     }
-    quick_sort(apples, N);
     fall(apples, falling, &falls_amount, 0, N);
     printf("%d\n", falls_amount);
     for (i = 0; i < N; i++) {
