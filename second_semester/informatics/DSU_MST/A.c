6c7cf218d3d016c2d55d6996fea6ac1a5244d8eb
Ilya Klopov <ilyaklopov07@yandex.ru>
2026-02-11 17:41:22 +0300

    #00000003 Prim works (kinda)

--- second_semester/informatics/DSU_MST/A.c
+++ second_semester/informatics/DSU_MST/A.c
@@ -1,17 +1,101 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int print_number(void) {
-    int number;
-    scanf("%d", &number);
-    if (number) {
-        print_number();
+#define MAX_WEIGHT 30001
+
+
+int get_minimal_weight(int* distances, int* visited, int N) {
+    if (!distances || !visited) {
+        return -1;
     }
-    printf("%d\n", number);
-    return 0;
+    int i = 0;
+    int minimum = MAX_WEIGHT;
+    int min_index = 0;
+    for (i = 0; i < N; i++) {
+        if (!visited[i] && distances[i] < minimum) {
+            minimum = distances[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+
+int** set_adjacency_matrix(int N, int M) {
+    int i;
+    int edge1;
+    int edge2;
+    int weight;
+    int** adjacency_matrix = calloc(N, sizeof(int*));
+    for (i = 0; i < N; i++) {
+        adjacency_matrix[i] = calloc(N, sizeof(int));
+    }
+    for (i = 0; i < M; i++) {
+        scanf("%d %d %d", &edge1, &edge2, &weight);
+        edge1--;
+        edge2--;
+        if (adjacency_matrix[edge1][edge2] == 0 || weight < adjacency_matrix[edge1][edge2]) {
+            adjacency_matrix[edge1][edge2] = weight;
+            adjacency_matrix[edge2][edge1] = weight;
+        }
+    }
+    return adjacency_matrix;
+}
+
+
+int Prim(int N, int M, int** adjacency_matrix) {
+    if (!adjacency_matrix) {
+        return -1;
+    }
+    int i;
+    int w;
+    int visited_amount = 1;
+    int answer = 0;
+    int v;
+    int* previous = calloc(N, sizeof(int));
+    for (i = 0; i < N; i++) {
+        previous[i] = -1;
+    }
+    int* distances = calloc(N, sizeof(int));
+    for (i = 0; i < N; i++) {
+        distances[i] = MAX_WEIGHT;
+    }
+    distances[0] = 0;
+    int* visited = calloc(N, sizeof(int));
+    visited[0] = 1;
+    while (visited_amount < M) {
+        for (i = 0; i < N; i++) {
+            v = get_minimal_weight(distances, visited, N);
+            for (w = 0; w < N; w++) {
+                if (adjacency_matrix[v][w]
+                    && !visited[w]
+                    && adjacency_matrix[v][w] < distances[w]) {
+                    previous[w] = v;
+                    distances[w] = adjacency_matrix[v][w];
+                    }
+            }
+        }
+        visited_amount++;
+    }
+    for (i = 0; i < N; i++) {
+        answer += distances[i];
+    }
+    free(distances);
+    free(visited);
+    free(previous);
+    for (i = 0; i < N; i++) {
+        free(adjacency_matrix[i]);
+    }
+    free(adjacency_matrix);
+    return answer;
 }
 
 
 int main(void) {
-    print_number();
+    int N;
+    int M;
+    scanf("%d %d", &N, &M);
+    int** adjacency_matrix = set_adjacency_matrix(N, M);
+    printf("%d", Prim(N, M, adjacency_matrix));
     return 0;
 }
