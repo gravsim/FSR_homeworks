b192d12c220a970bc81c923ce91ef28f2c561eec
manki <ilyaklopov07@yandex.ru>
2026-03-04 23:00:47 +0300

    #00000047

--- semester2/yandex/2026-02-19/A.c
+++ semester2/yandex/2026-02-19/A.c
@@ -2,18 +2,16 @@
 #include <stdlib.h>
 
 
-int print_way(int v, int i, int n, int* previous) {
-    if (i < 0 || i >= n) {
+int print_way(int v, int i, int n, int* previous, int* been) {
+    if (i < 0 || i >= n || been[i]) {
         return 0;
     }
     if (i == v) {
         printf("%d ", i);
         return 1;
     }
-    if (previous[i] == -1) {
-        return 0;
-    }
-    if (!print_way(v, previous[i], n, previous)) {
+    been[i] = 1;
+    if (!print_way(v, previous[i], n, previous, been)) {
         return 0;
     }
     printf("%d ", i);
@@ -23,30 +21,47 @@ int print_way(int v, int i, int n, int* previous) {
 
 int main(void) {
     int v;
-    int n = 10000;
+    int n;
     int command;
     int i;
-    int* previous = calloc(n, sizeof(int));
+    int* previous = NULL;
+    int* been = NULL;
     scanf("%d", &command);
     int to;
     while (command != 3) {
         switch (command) {
             case 1:
                 scanf("%d %d", &v, &n);
+                if (previous) {
+                    free(previous);
+                }
+                if (been) {
+                    free(been);
+                }
+                previous = calloc(n, sizeof(int));
+                been = calloc(n, sizeof(int));
                 for (i = 0; i < n; i++) {
                     scanf("%d", previous + i);
+                    been[i] = 0;
                 }
                 break;
             case 2:
                 scanf("%d", &to);
-                if (previous[to] == -1 || !print_way(v, to, n, previous)) {
+                if (!print_way(v, to, n, previous, been)) {
                     printf("-1");
                 }
+                printf("\n");
                 break;
             default:
                 break;
         }
         scanf("%d", &command);
+    }
+    if (previous) {
+        free(previous);
+    }
+    if (been) {
+        free(been);
     }
     return 0;
 }
