83bcb1dc9ce3bb48cf78511d8bf6e9860e5aab61
manki <ilyaklopov07@yandex.ru>
2026-03-11 15:16:17 +0300

    #00000063

--- semester2/informatics/2026-03-12 shortest_paths/C.c
+++ semester2/informatics/2026-03-12 shortest_paths/C.c
@@ -4,10 +4,14 @@
 
 
 #define HEAP_CAPACITY 1000
+#define MAX_DIRECTIONS 4
 
 
 typedef struct Node {
-    int index;
+    int x;
+    int y;
+    int direction;
+    int k;
     int weight;
 } Node;
 
@@ -17,6 +21,14 @@ typedef struct Heap {
     int capacity;
     Node* values;
 } Heap;
+
+
+int abs(int a) {
+    if (a < 0) {
+        return -a;
+    }
+    return a;
+}
 
 
 int swap_nodes(Node* a, Node* b) {
@@ -63,21 +75,23 @@ int sift_down(Heap* heap, int index) {
 }
 
 
-int push(Heap* heap, int index, int value) {
+int push(Heap* heap, int x, int y, int direction, int k, int weight) {
     if (is_full(heap)) {
         expand(heap);
     }
-    heap->values[heap->size].weight = value;
-    heap->values[heap->size].index = index;
+    heap->values[heap->size].weight = weight;
+    heap->values[heap->size].x = x;
+    heap->values[heap->size].y = y;
+    heap->values[heap->size].direction = direction;
+    heap->values[heap->size].k = k;
     sift_up(heap, heap->size);
     heap->size++;
     return 0;
 }
 
 
-int pop_minimum(Heap* heap, int* index, int* value) {
-    *value = heap->values[0].weight;
-    *index = heap->values[0].index;
+int pop_minimum(Heap* heap, Node* node) {
+    *node = heap->values[0];
     heap->values[0] = heap->values[--heap->size];
     return sift_down(heap, 0) + 1;
 }
@@ -92,54 +106,13 @@ int init_heap(Heap** heap) {
 }
 
 
-int is_right_turn(int* vector1, int* vector2) {
-    return vector1[0] * vector2[1] - vector2[0] * vector1[1] < 0;
+int is_right_turn(int old_direction, int new_direction) {
+    return new_direction == (old_direction + 1) % MAX_DIRECTIONS;
 }
 
 
-int check_vertex(Heap* heap,
-    int K,
-    int N,
-    int M,
-    int* direction,
-    int** connections,
-    int* distances,
-    int** directions,
-    int* right_turns,
-    int* visited,
-    int parent_x,
-    int parent_y) {
-
-    int y = parent_y + direction[0];
-    int x = parent_x + direction[1];
-    int index = y * M + x;
-    int parent_index = parent_y * M + parent_x;
-
-    if (x >= 0
-        && x < M
-        && y >= 0
-        && y < N
-        && connections[y][x] != 1
-        && distances[index] > distances[parent_index] + 1) {
-
-        if (is_right_turn(directions[parent_index], direction)) {
-            right_turns[index] = right_turns[parent_index] + 1;
-            if (right_turns[index] <= K) {
-                directions[index][0] = direction[0];
-                directions[index][1] = direction[1];
-                distances[index] = distances[parent_index] + 1;
-                push(heap, index, distances[index]);
-            }
-        } else {
-            right_turns[index] = right_turns[parent_index];
-            directions[index][0] = direction[0];
-            directions[index][1] = direction[1];
-            distances[index] = distances[parent_index] + 1;
-            push(heap, index, distances[index]);
-        }
-
-    }
-    return 1;
+int is_backward(int old_direction, int new_direction) {
+    return abs(old_direction - new_direction) == 2;
 }
 
 
@@ -149,36 +122,39 @@ int Dijkstra_algorithm(
     int M,
     Heap* heap,
     int** connections,
-    int V,
-    int* distances,
-    int** directions,
-    int* right_turns,
-    int* visited) {
-    if (!connections || !visited || !distances) {
+    int**** distances) {
+    if (!connections || !distances) {
         return -1;
     }
-    int v;
-    int value;
-    int paretn_x;
-    int parent_y;
-    int up[2] = {-1, 0};
-    int down[2] = {1, 0};
-    int right[2] = {0, 1};
-    int left[2] = {0, -1};
-
+    Node node;
+    int k;
+    int dir;
+    int new_x;
+    int new_y;
+    int dirs_x[] = {1, 0, -1, 0};
+    int dirs_y[] = {0, 1, 0, -1};
     while (heap->size > 0) {
-        pop_minimum(heap, &v, &value);
-        if (v == -1) {
-            return -1;
+        pop_minimum(heap, &node);
+        for (dir = 0; dir < MAX_DIRECTIONS; dir++) {
+            new_x = node.x + dirs_x[dir];
+            new_y = node.y + dirs_y[dir];
+            if (new_x >= 0
+            && new_x < M
+            && new_y >= 0
+            && new_y < N
+            && connections[new_y][new_x] != 1) {
+                k = node.k;
+                if (is_right_turn(node.direction, dir)) {
+                    k++;
+                }
+                if (!is_backward(node.direction, dir)
+                    && k <= K
+                    && distances[new_y][new_x][dir][k] > node.weight + 1) {
+                    distances[new_y][new_x][dir][k] = node.weight + 1;
+                    push(heap, new_x, new_y, dir, k, distances[new_y][new_x][dir][k]);
+                }
+            }
         }
-
-            paretn_x = v % M;
-            parent_y = v / M;
-            check_vertex(heap, K, N, M, up, connections, distances, directions, right_turns, visited, paretn_x, parent_y);
-            check_vertex(heap, K, N, M, down, connections, distances, directions, right_turns, visited, paretn_x, parent_y);
-            check_vertex(heap, K, N, M, right, connections, distances, directions, right_turns, visited, paretn_x, parent_y);
-            check_vertex(heap, K, N, M, left, connections, distances, directions, right_turns, visited, paretn_x, parent_y);
-
     }
     return 1;
 }
@@ -188,12 +164,16 @@ int main(void) {
     int K;
     int N;
     int M;
-    int to = -1;
-    int from = -1;
+    int from_x;
+    int from_y;
+    int to_x;
+    int to_y;
     int i;
     int j;
+    int k;
+    int l;
+    int dir;
     scanf("%d %d %d", &K, &N, &M);
-    int V = M * N;
     int** connections = calloc(N, sizeof(int*));
     for (i = 0; i < N; i++) {
         connections[i] = calloc(M, sizeof(int));
@@ -202,35 +182,48 @@ int main(void) {
         for (j = 0; j < M; j++) {
             scanf("%d", connections[i] + j);
             if (connections[i][j] == 2) {
-                from = i * M + j;
+                from_y = i;
+                from_x = j;
             } else if (connections[i][j] == 3) {
-                to = i * M + j;
+                to_y = i;
+                to_x = j;
             }
         }
     }
-    int* distances = calloc(V, sizeof(int));
-    for (i = 0; i < V; i++) {
-        distances[i] = INT_MAX;
+    int**** distances = calloc(N, sizeof(int***));
+    for (i = 0; i < N; i++) {
+        distances[i] = calloc(M, sizeof(int**));
+        for (j = 0; j < M; j++) {
+            distances[i][j] = calloc(MAX_DIRECTIONS, sizeof(int**));
+            for (k = 0; k < MAX_DIRECTIONS; k++) {
+                distances[i][j][k] = calloc(K+1, sizeof(int**));
+                for (l = 0; l < K+1; l++) {
+                    distances[i][j][k][l] = INT_MAX;
+                }
+            }
+
+        }
     }
-    distances[from] = 0;
-    int** directions = calloc(V, sizeof(int*));
-    for (i = 0; i < V; i++) {
-        directions[i] = calloc(2, sizeof(int));
-    }
-    int* right_turns = calloc(V, sizeof(int));
-    int* visited = calloc(V, sizeof(int));
     Heap* heap;
     init_heap(&heap);
-    directions[from][0] = 0;
-    directions[from][1] = 1;
-    push(heap, from, 0);
-    Dijkstra_algorithm(K, N, M, heap, connections, V, distances, directions, right_turns, visited);
-    if (distances[to] == INT_MAX) {
+    int answer = INT_MAX;
+    for (dir = 0; dir < MAX_DIRECTIONS; dir++) {
+        distances[from_y][from_x][dir][0] = 0;
+        push(heap, from_x, from_y, dir, 0, 0);
+    }
+    Dijkstra_algorithm(K, N, M, heap, connections, distances);
+    for (i = 0; i < MAX_DIRECTIONS; i++) {
+        for (j = 0; j < K+1; j++) {
+            if (answer > distances[to_y][to_x][i][j]) {
+                answer = distances[to_y][to_x][i][j];
+            }
+        }
+    }
+    if (answer == INT_MAX) {
         printf("-1");
     } else {
-        printf("%d", distances[to]);
+        printf("%d", answer);
     }
-
     free(distances);
     free(heap->values);
     free(heap);
