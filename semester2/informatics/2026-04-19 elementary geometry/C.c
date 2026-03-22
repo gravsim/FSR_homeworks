829e234f39db1dfc7636edab6d207a8f2102571a
manki <ilyaklopov07@yandex.ru>
2026-03-22 14:58:03 +0300

    #00000091

--- semester2/informatics/2026-04-19 elementary geometry/C.c
+++ semester2/informatics/2026-04-19 elementary geometry/C.c
@@ -3,12 +3,13 @@
 #include <math.h>
 
 
-#define EPSILON 1e-10
+#define EPSILON 1e-20
 
 
 double double_equal(double a, double b) {
     return fabs(a - b) <= EPSILON;
 }
+
 
 double cross2(double* a, double* b) {
     return a[0] * b[1] - a[1] * b[0];
@@ -16,13 +17,15 @@ double cross2(double* a, double* b) {
 
 
 int sign(double a) {
-    if (a < 0) {
+    if (double_equal(a, 0.)) {
+        return 0;
+    }
+    if (a < EPSILON) {
         return -1;
     }
-    if (a > 0) {
+    if (a > EPSILON) {
         return 1;
     }
-    return 0;
 }
 
 
@@ -30,11 +33,6 @@ int get_vec_sign(double* start, double* end, double* point) {
     double vector1[2] = {end[0] - start[0], end[1] - start[1]};
     double vector2[2] = {point[0] - start[0], point[1] - start[1]};
     return sign(cross2(vector1, vector2));
-}
-
-
-int point_equal(double* a, double* b) {
-    return double_equal(a[0], b[0]) && double_equal(a[1], b[1]);
 }
 
 
@@ -54,6 +52,11 @@ int point_on_segment(double* A, double* B, double* point) {
 }
 
 
+int on_same_side(double* A, double* B, double* point1, double* point2) {
+    return get_vec_sign(A, B, point1) == get_vec_sign(A, B, point2);
+}
+
+
 int main(void) {
     double* A1 = malloc(2 * sizeof(double));
     double* B1 = malloc(2 * sizeof(double));
@@ -62,12 +65,7 @@ int main(void) {
     double* B2 = malloc(2 * sizeof(double));
 
     scanf("%lf %lf %lf %lf %lf %lf %lf %lf", A1, A1 + 1, B1, B1 + 1, A2, A2 + 1, B2, B2 + 1);
-    if (point_equal(A1, A2)
-        || point_equal(A1, B2)
-        || point_equal(B1, A2)
-        || point_equal(B1, B2)
-
-        || point_on_segment(A1, B1, A2)
+    if (point_on_segment(A1, B1, A2)
         || point_on_segment(A1, B1, B2)
         || point_on_segment(A2, B2, A1)
         || point_on_segment(A2, B2, B1)
@@ -80,10 +78,7 @@ int main(void) {
         return 0;
     }
     int answer = 1;
-    if (get_vec_sign(A1, B1, A2) == get_vec_sign(A1, B1, B2)) {
-        answer = 0;
-    }
-    if (get_vec_sign(A2, B2, A1) == get_vec_sign(A2, B2, B1)) {
+    if (on_same_side(A1, B1, A2, B2) || on_same_side(A2, B2, A1, B1)) {
         answer = 0;
     }
     if (answer) {
