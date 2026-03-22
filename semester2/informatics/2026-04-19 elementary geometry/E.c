c944732e7704929c7b43c79caf369e2643a8c8b9
manki <ilyaklopov07@yandex.ru>
2026-03-22 19:41:28 +0300

    #00000096

--- semester2/informatics/2026-04-19 elementary geometry/E.c
+++ semester2/informatics/2026-04-19 elementary geometry/E.c
@@ -33,7 +33,7 @@ vec2* normalize(vec2* vector) {
 vec2* subtract(vec2* vector1, vec2* vector2) {
     vec2* difference = malloc(sizeof(vec2));
     difference->x = vector1->x - vector2->x;
-    difference->y = vector1->x - vector2->y;
+    difference->y = vector1->y - vector2->y;
     return difference;
 }
 
@@ -56,7 +56,7 @@ double dot(vec2* a, vec2* b) {
 }
 
 
-int int_sign(float a) {
+int int_sign(double a) {
     if (a < 0.) {
         return -1;
     } else if (a > 0.) {
@@ -93,7 +93,7 @@ int get_max_cos_index(vec2* vertices, int vert_amount, vec2* point1, vec2* point
     double max_cos = -2.;
     int max_index = -1;
     for (i = 0; i < vert_amount; i++) {
-        if (vec2_equal(vertices + i, point2)) {
+        if (!vec2_equal(vertices + i, point2)) {
             vec2* diff1 = subtract(point2, point1);
             vec2* diff2 = subtract(vertices + i, point2);
             double new_cos = get_cos(diff1, diff2);
@@ -107,39 +107,40 @@ int get_max_cos_index(vec2* vertices, int vert_amount, vec2* point1, vec2* point
 }
 
 
-void Jarvis_algotythm(int n, vec2* vertices, vec2* convex_vertices, int convex_size) {
+void Jarvis_algotythm(int n, vec2* vertices, vec2* convex_vertices, int* convex_size) {
     int i;
     int min_index = 0;
-    vec2 *point1;
-    vec2 *point2;
+    vec2 *point1 = malloc(sizeof(vec2));
+    vec2 *point2 = malloc(sizeof(vec2));
     for (i = 0; i < n; i++) {
         if (vertices[i].x < vertices[min_index].x || (double_equal(vertices[i].x, vertices[min_index].x) && vertices[i].y < vertices[min_index].y)){
             min_index = i;
         }
     }
     int current;
-    convex_size = 0;
-    convex_vertices[convex_size] = vertices[min_index];
-    convex_size++;
+    *convex_size = 0;
+    convex_vertices[*convex_size] = vertices[min_index];
+    (*convex_size)++;
 
-    point1 = convex_vertices;
-    point2 = malloc(sizeof(vec2));
+    point1->x = convex_vertices[0].x;
+    point1->y = convex_vertices[0].y;
+
     point2->x = point1->x;
-    point1->y = point1->y - 0.1;
+    point2->y = point1->y - 0.1;
     current = get_max_cos_index(vertices, n, point1, point2);
     do {
-        convex_vertices[convex_size] = vertices[current];
-        convex_size++;
-        point1 = convex_vertices + (convex_size-2);
-        point2 = convex_vertices + (convex_size-1);
+        convex_vertices[*convex_size] = vertices[current];
+        (*convex_size)++;
+        point1 = convex_vertices + (*convex_size-2);
+        point2 = convex_vertices + (*convex_size-1);
         current = get_max_cos_index(vertices, n, point1, point2);
     } while (current != -1 && current != min_index);
-    free(point2);
 }
 
 
 int main(void) {
     int n;
+    int answer = 0;
     vec2* barycenter = malloc(sizeof(vec2));
     scanf("%d %lf %lf", &n, &barycenter->x, &barycenter->y);
     vec2* vertices = calloc(n, sizeof(vec2));
@@ -149,7 +150,24 @@ int main(void) {
     for (i = 0; i < n; i++) {
         scanf("%lf %lf", &vertices[i].x, &vertices[i].y);
     }
-    Jarvis_algotythm(n ,vertices, convex_vertices, convex_size);
+    Jarvis_algotythm(n ,vertices, convex_vertices, &convex_size);
+    int next;
+    vec2* edge = malloc(sizeof(vec2));
+    vec2* vector = malloc(sizeof(vec2));
+    double t;
+    for (i = 0; i < convex_size; i++) {
+        next = (i + 1) % convex_size;
+        edge->x = convex_vertices[next].x - convex_vertices[i].x;
+        edge->y = convex_vertices[next].y - convex_vertices[i].y;
+
+        vector->x = barycenter->x - convex_vertices[i].x;
+        vector->y = barycenter->y - convex_vertices[i].y;
+        t = dot(edge, vector) / dot(edge, edge);
+        if (t > 0 && t < 1) {
+            answer++;
+        }
+    }
+    printf("%d", answer);
     free(barycenter);
     free(vertices);
     free(convex_vertices);
