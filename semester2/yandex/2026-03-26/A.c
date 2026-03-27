ec3f5e796db6dabaaf1c2f91395730e62fc95c7b
manki <ilyaklopov07@yandex.ru>
2026-03-27 23:02:33 +0300

    #00000114

--- semester2/yandex/2026-03-26/A.c
+++ semester2/yandex/2026-03-26/A.c
@@ -1,3 +1,4 @@
+#include <float.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -7,11 +8,17 @@
 
 
 typedef struct BST_node {
-    int value;
+    double y;
+    int line_index;
     struct BST_node* left;
     struct BST_node* right;
 } BST_node;
 
+
+
+int double_equal(double a, double b) {
+    return fabs(a - b) < EPSILON;
+}
 
 
 BST_node* BST_new_node(int value) {
@@ -19,7 +26,7 @@ BST_node* BST_new_node(int value) {
         Создаем новый узел.
     */
     BST_node* new_node = malloc(sizeof(BST_node));
-    new_node->value = value;
+    new_node->y = value;
     new_node->left = NULL;
     new_node->right = NULL;
     return new_node;
@@ -33,10 +40,10 @@ BST_node* BST_push(BST_node* root_p, int value) {
     if (!root_p) {
         return BST_new_node(value);
     }
-    if (value == root_p->value) {
+    if (double_equal(value, root_p->y)) {
         return root_p;
     }
-    if (root_p->value < value) {
+    if (root_p->y < value) {
         root_p->right = BST_push(root_p->right, value);
     } else {
         root_p->left = BST_push(root_p->left, value);
@@ -84,17 +91,46 @@ int BST_free_root(BST_node** root_pp) {
 }
 
 
+int get_lower_neighbour(BST_node* root_p, double value) {
+    double biggest = DBL_MIN;
+    int biggest_line_index = -1;
+    while (root_p) {
+        if (root_p->y > biggest && root_p->y < value) {
+            biggest = root_p->y;
+            biggest_line_index = root_p->line_index;
+        }
+        if (root_p->y < value) {
+            root_p = root_p->left;
+        } else {
+            root_p = root_p->right;
+        }
+    }
+    return biggest_line_index;
+}
+
+
+int get_higher_neighbour(BST_node* root_p, int value) {
+    double lowest = DBL_MAX;
+    int lowest_line_index = -1;
+    while (root_p) {
+        if (root_p->y < lowest && root_p->y > value) {
+            lowest = root_p->y;
+            lowest_line_index = root_p->line_index;
+        }
+        if (root_p->y > value) {
+            root_p = root_p->left;
+        } else {
+            root_p = root_p->right;
+        }
+    }
+    return lowest_line_index;
+}
 
 
 typedef struct vec2 {
     double x;
     double y;
 } vec2;
-
-
-double double_equal(double a, double b) {
-    return fabs(a - b) <= EPSILON;
-}
 
 
 double get_norm(vec2 vector) {
@@ -156,7 +192,7 @@ int vec2_equal(vec2 vector1, vec2 vector2) {
 }
 
 
-typedef struct Point {
+typedef struct Heap_node {
     /*
         type:
         0 - beginning of segment
@@ -166,13 +202,6 @@ typedef struct Point {
     vec2 coords;
     int line_index;
     int type;
-} Point;
-
-
-
-typedef struct Heap_node {
-    int index;
-    Point point;
 } Heap_node;
 
 
@@ -208,7 +237,7 @@ void Heap_expand(Heap* heap) {
 
 
 int Heap_sift_up(Heap* heap, int index) {
-    while (index > 0 && heap->values[index].point.coords.x < heap->values[(index - 1) / 2].point.coords.x) {
+    while (index > 0 && heap->values[index].coords.x < heap->values[(index - 1) / 2].coords.x) {
         Heap_swap_nodes(heap->values + index, heap->values + (index - 1) / 2);
         index = (index - 1) / 2;
     }
@@ -218,10 +247,10 @@ int Heap_sift_up(Heap* heap, int index) {
 
 int Heap_sift_down(Heap* heap, int index) {
     int max_index = index;
-    if (2 * index + 2 < heap->size && heap->values[2 * index + 2].point.coords.x < heap->values[max_index].point.coords.x) {
+    if (2 * index + 2 < heap->size && heap->values[2 * index + 2].coords.x < heap->values[max_index].coords.x) {
         max_index = 2 * index + 2;
     }
-    if (2 * index + 1 < heap->size && heap->values[2 * index + 1].point.coords.x < heap->values[max_index].point.coords.x) {
+    if (2 * index + 1 < heap->size && heap->values[2 * index + 1].coords.x < heap->values[max_index].coords.x) {
         max_index = 2 * index + 1;
     }
     if (max_index != index) {
@@ -232,21 +261,23 @@ int Heap_sift_down(Heap* heap, int index) {
 }
 
 
-int Heap_push(Heap* heap, int index, double value) {
+int Heap_push(Heap* heap, double x, double y, int line_index) {
     if (Heap_is_full(heap)) {
         Heap_expand(heap);
     }
-    heap->values[heap->size].point.coords.x = value;
-    heap->values[heap->size].index = index;
+    heap->values[heap->size].coords.x = x;
+    heap->values[heap->size].coords.y = y;
+    heap->values[heap->size].type = 0;
+    heap->values[heap->size].line_index = line_index;
     Heap_sift_up(heap, heap->size);
     heap->size++;
     return 0;
 }
 
 
-int Heap_pop_minimum(Heap* heap, int* index, Point* value) {
-    *value = heap->values[0].point;
-    *index = heap->values[0].index;
+int Heap_pop_minimum(Heap* heap, int* index, Heap_node* value) {
+    *value = heap->values[0];
+    *index = heap->values[0].line_index;
     heap->values[0] = heap->values[--heap->size];
     return Heap_sift_down(heap, 0) + 1;
 }
@@ -261,16 +292,54 @@ int Heap_init(Heap** heap) {
 }
 
 
+int is_intersection(Heap_node** segments, int i, int j) {
+    if (vectors_sign(segments[j][0].coords, segments[i][1].coords, segments[i][0].coords) !=
+        vectors_sign(segments[j][1].coords, segments[i][1].coords, segments[i][0].coords)
+        &&
+        vectors_sign(segments[i][0].coords, segments[j][1].coords, segments[j][0].coords) !=
+        vectors_sign(segments[i][1].coords, segments[j][1].coords, segments[j][0].coords)
+        ) {
+        return 1;
+    }
+    return 0;
+}
+
+
+vec2 get_intersection(Heap_node** segments, int i, int j) {
+    double x1 = segments[i][0].coords.x;
+    double x2 = segments[i][1].coords.x;
+    double x3 = segments[j][0].coords.x;
+    double x4 = segments[j][0].coords.x;
+
+    double y1 = segments[i][0].coords.y;
+    double y2 = segments[i][1].coords.y;
+    double y3 = segments[j][0].coords.y;
+    double y4 = segments[j][1].coords.y;
+
+    double denominator = (y2 - y1) * (x4 - x3) - (x2 - x1) * (y4 - y3);
+    double t = ((x4 - x3) * (y3 - y1) + (x1 - x3) * (y4 - y3)) / denominator;
+    return (vec2){x1 + t * (x2 - x1), y1 + t * (y2 - y1)};
+}
 
 
 
-void Bentley_Ottmann_algorithm(Heap* heap, BST_node* root_p) {
-    int index;
-    Point point;
+int get_neighbours(BST_node* root_p, double y, int* i, int* j) {
+    *i = get_higher_neighbour(root_p, y);
+    *j = get_lower_neighbour(root_p, y);
+    return 0;
+}
+
+
+void Bentley_Ottmann_algorithm(int n, Heap_node** segments, Heap* heap, BST_node* root_p) {
+    int line_index;
+    Heap_node point;
+    int i;
+    int j;
     while (!Heap_is_empty(heap)) {
-        Heap_pop_minimum(heap, &index, &point);
+        Heap_pop_minimum(heap, &line_index, &point);
         if (point.type == 0) {
             // Beginning
+            get_neighbours(root_p, point.coords.y, &i, &j);
             BST_push(root_p, point.line_index);
         } else if (point.type == 1) {
             // End
@@ -284,28 +353,39 @@ void Bentley_Ottmann_algorithm(Heap* heap, BST_node* root_p) {
 
 int main(void) {
     int n;
-    int answer = 0;
-    vec2* barycenter = malloc(sizeof(vec2));
-    scanf("%d %lf %lf", &n, &barycenter->x, &barycenter->y);
-    vec2* vertices = calloc(n, sizeof(vec2));
-    vec2* convex_vertices = calloc(n, sizeof(vec2));
-    int convex_size = 0;
+
+    scanf("%d", &n);
+    int line_index;
+    double Ax;
+    double Ay;
+    double Bx;
+    double By;
+    int intersections = 0;
     int i;
     BST_node* root = NULL;
     Heap* heap;
+    Heap_node** segments = calloc(n, sizeof(Heap_node*));
+    for (i = 0; i < n; i++) {
+        segments[i] = calloc(2, sizeof(Heap_node));
+    }
     Heap_init(&heap);
     for (i = 0; i < n; i++) {
-        scanf("%lf %lf", &vertices[i].x, &vertices[i].y);
-    }
-    Bentley_Ottmann_algorithm(heap, root);
-    int next;
-    vec2 edge;
-    vec2 vector;
-    double t;
+        scanf("%d %lf %lf %lf %lf", &line_index, &Ax, &Ay, &Bx, &By);
+        segments[i][0].line_index = line_index;
+        segments[i][0].coords.x = Ax;
+        segments[i][0].coords.y = Ay;
+        segments[i][0].type = 0;
 
-    printf("%d", answer);
-    free(barycenter);
-    free(vertices);
-    free(convex_vertices);
+        segments[i][1].line_index = line_index;
+        segments[i][1].coords.x = Bx;
+        segments[i][1].coords.y = By;
+        segments[i][1].type = 1;
+
+        Heap_push(heap, Ax, Ay, line_index);
+        Heap_push(heap, Bx, By, line_index);
+    }
+    Bentley_Ottmann_algorithm(n, segments, heap, root);
+
+    printf("%d", intersections);
     return 0;
 }
