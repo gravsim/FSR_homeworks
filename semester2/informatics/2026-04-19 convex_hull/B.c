743b113844f8dbc2cdc8ae17e584f7f1cb342dd4
manki <ilyaklopov07@yandex.ru>
2026-04-01 16:30:32 +0300

    #00000127

---
+++ semester2/informatics/2026-04-19 convex_hull/B.c
@@ -0,0 +1,178 @@
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+#define EPSILON 1e-300
+
+
+typedef struct vec2 {
+    double x;
+    double y;
+} vec2;
+
+
+int double_equal(double a, double b) {
+    return fabs(a - b) <= EPSILON;
+}
+
+
+double get_norm(vec2 vector) {
+    return sqrt(vector.x * vector.x + vector.y * vector.y);
+}
+
+
+vec2 subtract(vec2 vector1, vec2 vector2) {
+    return (vec2){vector1.x - vector2.x, vector1.y - vector2.y};
+}
+
+
+double distance(vec2 vector1, vec2 vector2) {
+    return get_norm(subtract(vector2, vector1));
+}
+
+
+vec2 normalize(vec2 vector) {
+    double norm = get_norm(vector);
+    if (double_equal(norm, 0.)) {
+        return vector;
+    }
+    return (vec2){vector.x / norm, vector.y / norm};
+}
+
+
+double cross2(vec2 a, vec2 b) {
+    return a.x * b.y - a.y * b.x;
+}
+
+
+int double_sign(double a) {
+    return (a > EPSILON) - (a < -EPSILON);
+}
+
+
+int vectors_sign(vec2 point, vec2 start, vec2 end) {
+    vec2 edge = subtract(end, start);
+    vec2 diff = subtract(point, start);
+    return double_sign(cross2(edge, diff));
+}
+
+
+int point_on_segment(vec2 A, vec2 B, vec2 point) {
+    return double_equal(distance(A, point) + distance(B, point), distance(A, B));
+}
+
+
+double dot(vec2 a, vec2 b) {
+    return a.x * b.x + a.y * b.y;
+}
+
+
+double get_cos(vec2 vector1, vec2 vector2) {
+    return dot(normalize(vector1), normalize(vector2));
+}
+
+
+int vec2_equal(vec2 vector1, vec2 vector2) {
+    return double_equal(vector1.x, vector2.x)
+           &&
+           double_equal(vector1.y, vector2.y);
+}
+
+
+int get_max_cos_index(vec2* vertices, int vert_amount, vec2 point1, vec2 point2) {
+    if (vertices == NULL) {
+        return -1;
+    }
+    int i;
+    double max_cos = -2.;
+    int max_index = -1;
+    vec2 diff1 = subtract(point2, point1);
+    vec2 diff2;
+    double new_cos;
+    for (i = 0; i < vert_amount; i++) {
+        if (!vec2_equal(vertices[i], point2)) {
+            diff2 = subtract(vertices[i], point2);
+            new_cos = get_cos(diff1, diff2);
+            if (double_equal(max_cos, new_cos)) {
+                if (max_index == -1
+                    ||
+                    get_norm(diff2) > distance(point2, vertices[max_index])) {
+                        max_index = i;
+                    }
+            } else if (new_cos > max_cos + EPSILON) {
+                max_cos = new_cos;
+                max_index = i;
+            }
+        }
+    }
+    return max_index;
+}
+
+
+
+int Jarvis_algorithm(
+    int n,
+    vec2* vertices,
+    vec2* convex_vertices,
+    int* convex_size
+    ) {
+    if (vertices == NULL
+        ||
+        convex_vertices == NULL
+        ||
+        convex_size == NULL) {
+        return -1;
+    }
+    int i;
+    int min_index = 0;
+    for (i = 0; i < n; i++) {
+        if (vertices[i].x > vertices[min_index].x
+            ||
+            (double_equal(vertices[i].x, vertices[min_index].x)
+             &&
+             vertices[i].y < vertices[min_index].y)){
+                 min_index = i;
+             }
+    }
+    int current;
+    *convex_size = 0;
+    convex_vertices[*convex_size] = vertices[min_index];
+    (*convex_size)++;
+
+    vec2 point1= convex_vertices[0];
+    vec2 point2 = (vec2){point1.x, point1.y - 1.0};
+    current = get_max_cos_index(vertices, n, point2, point1);
+    while (*convex_size < n && current != -1 && current != min_index) {
+        convex_vertices[*convex_size] = vertices[current];
+        (*convex_size)++;
+        point1 = convex_vertices[*convex_size-2];
+        point2 = convex_vertices[*convex_size-1];
+        current = get_max_cos_index(vertices, n, point1, point2);
+    }
+    return 1;
+}
+
+
+int main(void) {
+    long n;
+    vec2 point;
+    scanf("%ld %lf %lf", &n, &point.x, &point.y);
+    vec2* vertices = calloc(n, sizeof(vec2));
+    vec2* convex_vertices = calloc(n, sizeof(vec2));
+    int convex_size = 0;
+    long i;
+    long intersections_amount = 0;
+    for (i = 0; i < n; i++) {
+        scanf("%lf %lf", &vertices[i].x, &vertices[i].y);
+    }
+    Jarvis_algorithm(n ,vertices, convex_vertices, &convex_size);
+    if (i < n || intersections_amount % 2 == 1) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+    free(vertices);
+    return 0;
+}
