7974790516892e52626561e14c80c516f2d3d384
manki <ilyaklopov07@yandex.ru>
2026-04-01 18:21:20 +0300

    #00000133

--- semester2/informatics/2026-04-19_2_convex_hull/D.c
+++ semester2/informatics/2026-04-19_2_convex_hull/D.c
@@ -1,3 +1,4 @@
+#include <float.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -58,60 +59,181 @@ int vec2_equal(vec2 vector1, vec2 vector2) {
 }
 
 
-int get_max_cos_index(vec2* vertices, int vert_amount, vec2 point1, vec2 point2) {
-    if (vertices == NULL) {
-        return -1;
-    }
-    int i;
-    double max_cos = -2.;
-    int max_index = -1;
-    vec2 diff1 = subtract(point2, point1);
-    vec2 diff2;
-    double new_cos;
-    for (i = 0; i < vert_amount; i++) {
-        if (!vec2_equal(vertices[i], point2)) {
-            diff2 = subtract(vertices[i], point2);
-            new_cos = get_cos(diff1, diff2);
-            if (double_equal(max_cos, new_cos)) {
-                if (max_index == -1
-                    ||
-                    get_norm(diff2) > distance(point2, vertices[max_index])) {
-                        max_index = i;
-                    }
-            } else if (new_cos > max_cos + EPSILON) {
-                max_cos = new_cos;
-                max_index = i;
-            }
-        }
-    }
-    return max_index;
+double cross2(vec2 a, vec2 b) {
+    return a.x * b.y - a.y * b.x;
+}
+
+int double_sign(double a) {
+    return (a > EPSILON) - (a < -EPSILON);
+}
+
+
+int vectors_sign(vec2 point, vec2 start, vec2 end) {
+    vec2 edge = subtract(end, start);
+    vec2 diff = subtract(point, start);
+    return double_sign(cross2(edge, diff));
 }
 
 
 
+int point_on_segment(vec2 A, vec2 B, vec2 point) {
+    return double_equal(distance(A, point) + distance(B, point), distance(A, B));
+}
+
+
+int get_polygon_area(long fence_length, vec2* fence, double* area) {
+    if (fence == NULL) {
+        return -1;
+    }
+    int i;
+    int next;
+    *area = 0;
+    for (i = 0; i < fence_length; i++) {
+        next = (i + 1) % fence_length;
+        *area += fence[i].x * fence[next].y
+                -
+                fence[i].y * fence[next].x;
+
+    }
+    *area = fabs(*area) / 2;
+    return 1;
+}
+
+
+int swap_vec2_pointers(vec2** a, vec2** b) {
+    if (a == NULL || b == NULL) {
+        return -1;
+    }
+    vec2* tmp = *a;
+    *a = *b;
+    *b = tmp;
+    return 1;
+}
+
+
+int swap_double(double* a, double* b) {
+    if (a == NULL || b == NULL) {
+        return -1;
+    }
+    double tmp = *a;
+    *a = *b;
+    *b = tmp;
+    return 1;
+}
+
+
+int quick_sort(double* main_array, vec2** side_array, int size, int down, int up) {
+    if (main_array == NULL || side_array == NULL) {
+        return -1;
+    }
+    if (down >= up) {
+        return 0;
+    }
+    double pivot = main_array[(up + down) / 2];
+    int left = down;
+    int right = up;
+    int i;
+    while (left <= right) {
+        while (main_array[left] < pivot) {
+            left++;
+        }
+        while (pivot < main_array[right]) {
+            right--;
+        }
+        if (left <= right) {
+            swap_double(main_array + left, main_array + right);
+            for (i = 0; i < size; i++) {
+                swap_vec2_pointers(side_array + left, side_array + right);
+            }
+            left++;
+            right--;
+        }
+    }
+    quick_sort(main_array, side_array, size, down, right);
+    quick_sort(main_array, side_array, size, left, up);
+    return 1;
+}
+
+
+int in_polygon(long vertices_amount, vec2* vertices, vec2 point) {
+    int i = 0;
+    int end = 1;
+    vec2 ray_end = (vec2){10000, point.y + 10};
+    int intersections_amount = 0;
+    while (i < vertices_amount && !point_on_segment(vertices[i], vertices[end], point)) {
+        end = (i + 1) % vertices_amount;
+        if (vectors_sign(vertices[i], point, ray_end)
+            !=
+            vectors_sign(vertices[end], point, ray_end)
+            &&
+            vectors_sign(point, vertices[i], vertices[end])
+            !=
+            vectors_sign(ray_end, vertices[i], vertices[end])
+            ) {
+            intersections_amount++;
+            }
+        i++;
+    }
+    return i < vertices_amount || intersections_amount % 2 == 1;
+}
+
 
 int main(void) {
-    long n;
-    int convex_size = 0;
+    long N;
+    long M;
     long i;
-    double area = 0;
-    double perimeter = 0;
-    int next;
-    scanf("%ld", &n);
-    vec2* vertices = calloc(n, sizeof(vec2));
-    vec2* convex_vertices = calloc(n, sizeof(vec2));
-    for (i = 0; i < n; i++) {
-        scanf("%lf %lf", &vertices[i].x, &vertices[i].y);
+    long j;
+    long k;
+    scanf("%ld", &N);
+    vec2** fences = calloc(N, sizeof(vec2*));
+    long* fences_lengths = calloc(N, sizeof(long));
+    double* areas = calloc(N, sizeof(double));
+    double* max_x = calloc(N, sizeof(double));
+    for (i = 0; i < N; i++) {
+        scanf("%ld", fences_lengths + i);
+        fences[i] = calloc(fences_lengths[i], sizeof(vec2));
+        for (j = 0; j < fences_lengths[i]; j++) {
+            scanf("%lf %lf", &fences[i][j].x, &fences[i][j].y);
+        }
+        get_polygon_area(fences_lengths[i], fences[i], areas + i);
     }
-    for (i = 0; i < convex_size; i++) {
-        next = (i + 1) % convex_size;
-        area += convex_vertices[i].x * convex_vertices[next].y
-                -
-                convex_vertices[i].y * convex_vertices[next].x;
-        perimeter += distance(convex_vertices[i], convex_vertices[next]);
+    for (i = 0; i < N; i++) {
+        max_x[i] = DBL_MIN;
+        for (j = 0; j < fences_lengths[i]; j++) {
+            if (fences[i][j].x > max_x[i]) {
+                max_x[i] = fences[i][j].x;
+            }
+        }
     }
-    area = fabs(area) / 2;
-    printf("%lf %lf", perimeter, area);
-    free(vertices);
+    quick_sort(max_x, fences, N, 0, N - 1);
+    scanf("%ld", &M);
+    vec2* invaders = calloc(M, sizeof(vec2));
+    for (i = 0; i < N; i++) {
+        scanf("%lf %lf", &invaders[i].x, &invaders[i].y);
+    }
+    int invaded;
+    int l;
+    double invaded_area = 0;
+    for (k = 0; k < M; k++) {
+        l = 0;
+        invaded = 1;
+        while (l < N && invaded) {
+            if (in_polygon(fences_lengths[l], fences[l], invaders[k])) {
+                invaded = 0;
+                invaded_area += areas[l];
+                if (l > 0) {
+                    invaded_area -= areas[l - 1];
+                }
+            }
+            l++;
+        }
+    }
+    printf("%lf", invaded_area);
+    for (i = 0; i < N; i++) {
+        free(fences[i]);
+    }
+    free(fences_lengths);
+    free(areas);
+    free(max_x);
     return 0;
 }
