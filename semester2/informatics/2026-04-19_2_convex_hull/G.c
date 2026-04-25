40cc0fb4112460e206034b41bcc59bf63e13a695
manki <ilyaklopov07@yandex.ru>
2026-04-26 00:39:25 +0300

    #00000159

--- semester2/informatics/2026-04-19_2_convex_hull/G.c
+++ semester2/informatics/2026-04-19_2_convex_hull/G.c
@@ -17,36 +17,8 @@ int double_equal(double a, double b) {
 }
 
 
-double get_norm(vec2 vector) {
-    return sqrt(vector.x * vector.x + vector.y * vector.y);
-}
-
-
 vec2 subtract(vec2 vector1, vec2 vector2) {
     return (vec2){vector1.x - vector2.x, vector1.y - vector2.y};
-}
-
-
-double distance(vec2 vector1, vec2 vector2) {
-    return get_norm(subtract(vector2, vector1));
-}
-
-vec2 normalize(vec2 vector) {
-    double norm = get_norm(vector);
-    if (double_equal(norm, 0.)) {
-        return vector;
-    }
-    return (vec2){vector.x / norm, vector.y / norm};
-}
-
-
-double dot(vec2 a, vec2 b) {
-    return a.x * b.x + a.y * b.y;
-}
-
-
-double get_cos(vec2 vector1, vec2 vector2) {
-    return dot(normalize(vector1), normalize(vector2));
 }
 
 
@@ -57,37 +29,7 @@ int vec2_equal(vec2 vector1, vec2 vector2) {
 }
 
 
-int get_max_cos_index(vec2* vertices, int vert_amount, vec2 point1, vec2 point2) {
-    if (vertices == NULL) {
-        return -1;
-    }
-    int i;
-    double max_cos = -2.;
-    int max_index = -1;
-    vec2 diff1 = subtract(point2, point1);
-    vec2 diff2;
-    double new_cos;
-    for (i = 0; i < vert_amount; i++) {
-        if (!vec2_equal(vertices[i], point2)) {
-            diff2 = subtract(vertices[i], point2);
-            new_cos = get_cos(diff1, diff2);
-            if (double_equal(max_cos, new_cos)) {
-                if (max_index == -1
-                    ||
-                    get_norm(diff2) > distance(point2, vertices[max_index])) {
-                        max_index = i;
-                    }
-            } else if (new_cos > max_cos + PRECISION) {
-                max_cos = new_cos;
-                max_index = i;
-            }
-        }
-    }
-    return max_index;
-}
-
-
-int swap_vec2(vec2* a, vec2* b) {
+int swap_vec2(vec2 *a, vec2 *b) {
     if (!a || !b) {
         return -1;
     }
@@ -109,65 +51,74 @@ int vec2_smaller(vec2 a, vec2 b) {
 }
 
 
-void quick_sort(vec2* main_array, int size, int down, int up) {
-    if (down >= up) {
-        return;
-    }
-    vec2 pivot = main_array[(up + down) / 2];
-    int left = down;
-    int right = up;
-    while (left <= right) {
-        while (vec2_smaller(main_array[left], pivot)) {
-            left++;
-        }
-        while (vec2_smaller(pivot, main_array[right])) {
-            right--;
-        }
-        if (left <= right) {
-            swap_vec2(main_array + left, main_array + right);
-            left++;
-            right--;
-        }
-    }
-    quick_sort(main_array, size, down, right);
-    quick_sort(main_array, size, left, up);
+double cross2(vec2 a, vec2 b) {
+    return a.x * b.y - a.y * b.x;
 }
 
 
-int Jarvis_algorithm(
+void vec2_quick_sort(vec2 *array, int size, int down, int up) {
+    if (down >= up) {
+        return;
+    }
+    vec2 pivot = array[(up + down) / 2];
+    int left = down;
+    int right = up;
+    while (left <= right) {
+        while (vec2_smaller(array[left], pivot)) {
+            left++;
+        }
+        while (vec2_smaller(pivot, array[right])) {
+            right--;
+        }
+        if (left <= right) {
+            swap_vec2(array + left, array + right);
+            left++;
+            right--;
+        }
+    }
+    vec2_quick_sort(array, size, down, right);
+    vec2_quick_sort(array, size, left, up);
+}
+
+
+int double_sign(double a) {
+    return (a > PRECISION) - (a < -PRECISION);
+}
+
+
+int vectors_sign(vec2 point, vec2 start, vec2 end) {
+    vec2 edge = subtract(end, start);
+    vec2 diff = subtract(point, start);
+    return double_sign(cross2(edge, diff));
+}
+
+
+int Graham_algorithm(
     int n,
-    vec2* polygon,
-    vec2* convex_vertices,
-    int* convex_size,
-    int max_x_index,
-    int min_y_index
-    ) {
+    vec2 *polygon,
+    vec2 *convex_vertices,
+    int *convex_size
+) {
     if (polygon == NULL
         ||
         convex_vertices == NULL
         ||
         convex_size == NULL) {
-            return -1;
+        return -1;
     }
-    int current;
     *convex_size = 0;
-    convex_vertices[*convex_size] = polygon[min_y_index];
-    (*convex_size)++;
+    int i;
+    for (i = 0; i < n; i++) {
+        while (*convex_size > 1
+            &&
+            vectors_sign(polygon[i],
+            convex_vertices[*convex_size - 2],
+            convex_vertices[*convex_size - 1]) <= 0) {
+            (*convex_size)--;
+        }
+        convex_vertices[(*convex_size)++] = polygon[i];
+    }
 
-    vec2 point1 = convex_vertices[0];
-    vec2 point2 = (vec2){point1.x, point1.y + 1.0};
-    current = get_max_cos_index(polygon, n, point2, point1);
-    while (*convex_size < n && current != -1 && current != max_x_index) {
-        convex_vertices[*convex_size] = polygon[current];
-        (*convex_size)++;
-        point1 = convex_vertices[*convex_size-2];
-        point2 = convex_vertices[*convex_size-1];
-        current = get_max_cos_index(polygon, n, point1, point2);
-    }
-    if (current == max_x_index) {
-        convex_vertices[*convex_size] = polygon[current];
-        (*convex_size)++;
-    }
     return 1;
 }
 
@@ -179,7 +130,7 @@ int main(void) {
     int i;
     int j = 0;
     scanf("%d", &cuts_amount);
-    vec2* polygon = calloc(polygon_size, sizeof(vec2));
+    vec2 *polygon = calloc(polygon_size, sizeof(vec2));
     int points_amount = 0;
     for (i = 0; i < cuts_amount; i++) {
         scanf("%d", &cut_size);
@@ -190,45 +141,29 @@ int main(void) {
             points_amount++;
         }
     }
-    vec2* convex_vertices = calloc(polygon_size, sizeof(vec2));
+    vec2 *convex_vertices = calloc(polygon_size, sizeof(vec2));
     int convex_size = 0;
 
     for (i = 0; i < polygon_size; i++) {
         polygon[i].x = fabs(polygon[i].x);
     }
-    vec2* unique_polygon = calloc(polygon_size, sizeof(vec2));
+    vec2 *unique_polygon = calloc(polygon_size, sizeof(vec2));
     int unique_polygon_size = 0;
-    quick_sort(polygon,
-        polygon_size,
-        0,
-        polygon_size - 1);
+    vec2_quick_sort(polygon,
+                    polygon_size,
+                    0,
+                    polygon_size - 1);
     unique_polygon[unique_polygon_size++] = polygon[0];
     for (i = 1; i < polygon_size; i++) {
         if (!vec2_equal(polygon[i - 1], polygon[i])) {
             unique_polygon[unique_polygon_size++] = polygon[i];
         }
     }
-    int max_x_index = -1;
-    int min_y_index = -1;
-    for (i = 0; i < unique_polygon_size; i++) {
-        if (double_equal(unique_polygon[i].x, 0)
-            &&
-            (min_y_index == -1 || unique_polygon[i].y < unique_polygon[min_y_index].y)) {
-            min_y_index = i;
-            }
-        if (double_equal(unique_polygon[i].y, 0)
-            &&
-            (max_x_index == -1 || unique_polygon[i].x < unique_polygon[max_x_index].x)) {
-            max_x_index = i;
-            }
-    }
-    Jarvis_algorithm(unique_polygon_size,
-        unique_polygon,
-        convex_vertices,
-        &convex_size,
-        max_x_index,
-        min_y_index
-        );
+    Graham_algorithm(unique_polygon_size,
+                     unique_polygon,
+                     convex_vertices,
+                     &convex_size
+    );
     double A;
     double B;
     double C;
