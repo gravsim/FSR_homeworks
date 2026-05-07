5054fb000b141ba4d1fd4872446415d38f4a7b0c
manki <ilyaklopov07@yandex.ru>
2026-05-07 23:45:39 +0300

    #00000203

--- semester2/informatics/2026-05-20_1_strings/C.c
+++ semester2/informatics/2026-05-20_1_strings/C.c
@@ -2,21 +2,135 @@
 #include <stdlib.h>
 
 
-#define MAX_LENGTH 1000000
+#define MAX_LENGTH 10000
+#define ALPHABET_SIZE 26
 
 
-int prefix_function(char* string, int* pi, int length) {
+struct TrieNode {
+    struct TrieNode** children;
+    int end_of_word;
+} typedef TrieNode;
+
+
+
+int get_index(char symbol) {
+    return symbol - 'a';
+}
+
+
+char get_char(int index) {
+    return 'a' + index;
+}
+
+
+TrieNode** Trie_search_node(TrieNode** current, char* word, int length) {
+    int i = 0;
+    int index;
+    while (i < length && *current) {
+        index = get_index(word[i]);
+        current = &(*current)->children[index];
+        i++;
+    }
+    if (i < length) {
+        return NULL;
+    }
+    return current;
+}
+
+
+int Trie_delete_node(TrieNode** node) {
+    if (!node || !*node || !(*node)->end_of_word) {
+        return 0;
+    }
+    (*node)->end_of_word = 0;
+    return 1;
+}
+
+
+TrieNode* Trie_new_node(void) {
+    TrieNode* new_node = malloc(sizeof(TrieNode));
+    new_node->children = calloc(ALPHABET_SIZE, sizeof(TrieNode*));
+    new_node->end_of_word = 0;
+    return new_node;
+}
+
+
+TrieNode* Trie_push(TrieNode* root, char* word, int length) {
+    if (root == NULL || word == NULL) {
+        root = Trie_new_node();
+    }
+    TrieNode* current = root;
     int i;
-    int k;
-    for (i = 1; i < length; i++) {
-        k = pi[i - 1];
-        while (k > 0 && string[k] != string[i]) {
-            k = pi[k - 1];
+    int index;
+    for (i = 0; i < length; i++) {
+        index = get_index(word[i]);
+        if (!current->children[index]) {
+            current->children[index] = Trie_new_node();
         }
-        if (string[k] == string[i]) {
-            k++;
+        current = current->children[index];
+    }
+    current->end_of_word = 1;
+    return root;
+}
+
+
+int Trie_free_node(TrieNode** node) {
+    if (node == NULL || *node == NULL) {
+        return 0;
+    }
+    int i;
+    for (i = 0; i < ALPHABET_SIZE; i++) {
+        if ((*node)->children[i]) {
+            Trie_free_node(&(*node)->children[i]);
         }
-        pi[i] = k;
+    }
+    free((*node)->children);
+    free(*node);
+    *node = NULL;
+    return 1;
+}
+
+
+int print_string(char* string, int length) {
+    if (string == NULL) {
+        return 0;
+    }
+    int i;
+    for (i = 0; i < length; i++) {
+        printf("%c", string[i]);
+    }
+    printf(" ");
+    return 1;
+}
+
+
+int Trie_print(TrieNode* node, char* sample, int length) {
+    if (node == NULL || sample == NULL) {
+        return 0;
+    }
+    int i;
+    for (i = 0; i < ALPHABET_SIZE; i++) {
+        if (node->children[i]) {
+            sample[length] = get_char(i);
+            Trie_print(node->children[i], sample, length + 1);
+            if (node->children[i]->end_of_word) {
+                print_string(sample, length + 1);
+            }
+        }
+    }
+    return 1;
+}
+
+
+int read_word(int* length, char* string) {
+    if (length == NULL || string == NULL) {
+        return 0;
+    }
+    *length = 0;
+    scanf(" %c", string + *length);
+    (*length)++;
+    while (scanf("%c", string + *length) != EOF && string[*length] != '\n') {
+        (*length)++;
     }
     return 1;
 }
@@ -26,21 +140,17 @@ int main(void) {
     int amount;
     int max_prefix;
     scanf("%d %d", &amount, &max_prefix);
-    int* lengths = calloc(amount, sizeof(int));
-    char** words = calloc(amount, sizeof(char*));
     int i;
-    for (i = 1; i < amount; i++) {
-        words[i] = calloc(200, sizeof(char));
-        while (scanf("%c", words[i] + lengths[i]) != EOF && words[i][lengths[i]] != '\n') {
-            lengths[i]++;
-        }
+    TrieNode* root = NULL;
+    int length = 0;
+    char string[MAX_LENGTH];
+    TrieNode** found;
+    char* sample = calloc(MAX_LENGTH, sizeof(char));
+    for (i = 0; i < amount; i++) {
+        read_word(&length, string);
+        root = Trie_push(root, string, length);
     }
-
-    int* pi = calloc(length, sizeof(int));
-    prefix_function(string, pi, length);
-    int max_index = pi[length - 1];
-    int period = length - max_index;
-    printf("%d ", length % period == 0 ? length / period : 1);
-    free(pi);
+    Trie_print(root, sample, 0);
+    Trie_free_node(&root);
     return 0;
 }
