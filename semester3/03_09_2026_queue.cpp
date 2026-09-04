b2fb923e77839568ce3bfd5514ebe51a603289df
manki <ilyaklopov07@yandex.ru>
2026-09-04 22:05:26 +0300

    #00000232

--- semester3/03_09_2026_queue.cpp
+++ semester3/03_09_2026_queue.cpp
@@ -4,13 +4,14 @@
 struct Node
 {
     char value = 0;
-    Node* next = nullptr;
+    Node* previous = nullptr;
 };
 
 
 struct Queue
 {
     int size = 0;
+    int max_size = 0;
     Node* start = nullptr;
     Node* end = nullptr;
 };
@@ -19,14 +20,15 @@ struct Queue
 int push(Queue* queue, char value) {
     Node* new_node = new Node;
     new_node->value = value;
-    new_node->next = queue->end;
-    if (queue->start == nullptr) {
-        queue->start = new_node;
+    new_node->previous = nullptr;
+    queue->size++;
+    if (queue->end == nullptr) {
         queue->end = new_node;
+        queue->start = new_node;
         return -1;
     }
+    queue->end->previous = new_node;
     queue->end = new_node;
-    queue->size++;
     return 0;
 }
 
@@ -36,36 +38,52 @@ int pop(Queue* queue, char* value) {
         return -1;
     }
     *value = queue->start->value;
-    Node* next = queue->end->next;
-    delete queue->end;
-    queue->end = next;
+    Node* previous = queue->start->previous;
+    delete queue->start;
+    queue->start = previous;
     queue->size--;
     return 0;
 }
 
 
 bool is_empty(Queue* queue) {
-    return queue->size > 0;
+    return queue->size == 0;
 }
 
 
-void clear(Queue* queue) {
+void clear(Queue* queue, bool fill_zeros = false) {
     Node* current = queue->end;
-    Node* next;
+    Node* previous;
     while (current) {
-        next = current->next;
-        delete current;
-        current = next;
+        previous = current->previous;
+        if (fill_zeros) {
+            current->value = 0;
+        } else {
+            delete current;
+        }
+        current = previous;
     }
 }
 
 
+int get_size(Queue* queue) {
+    return queue->size;
+}
+
+
+void init_queue(Queue* queue, int max_size = 100) {
+    queue->max_size = max_size;
+}
+
+
 int main() {
-    int len;
     int command;
     char value;
-    std::cin >> len;
+    int max_size;
     Queue* queue = new Queue;
+    std::cout << "Enter maximum size of queue: ";
+    std::cin >> max_size;
+    init_queue(queue, max_size);
     do {
         std::cin >> command;
         switch (command) {
@@ -74,14 +92,28 @@ int main() {
                 push(queue, value);
                 break;
             case 2:
-                pop(queue, &value);
-                std::cout << value << "\n";
+                if (is_empty(queue)) {
+                    std::cout << "Queue is empty" << "\n";
+                } else {
+                    pop(queue, &value);
+                    std::cout << value << "\n";
+                }
+                break;
+            case 3:
+                if (is_empty(queue)) {
+                    std::cout << "Queue is empty" << "\n";
+                } else {
+                    std::cout << "Queue is not empty" << "\n";
+                }
                 break;
             case 4:
-                std::cout << is_empty(queue) ? "Empty" : "Not empty" << "\n";
+                std::cout << get_size(queue) << "\n";
                 break;
             case 5:
                 clear(queue);
+                break;
+            case 6:
+                clear(queue, true);
                 break;
             default:
                 break;
