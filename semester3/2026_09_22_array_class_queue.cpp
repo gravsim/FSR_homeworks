3a7bc5fc2d2ce8eadfab1eb6d0a80f4df9b67573
manki <ilyaklopov07@yandex.ru>
2026-09-22 23:59:01 +0300

    #00000239

--- semester3/2026_09_22_array_class_queue.cpp
+++ semester3/2026_09_22_array_class_queue.cpp
@@ -5,23 +5,20 @@
 #define QUEUE_FULL 1
 
 
-struct Node
-{
-    char value;
-    Node* next;
-};
-
-
 class Queue
 {
 private:
     int size;
     int max_size;
-    Node* start;
-    Node* end;
+    int* values;
+    int start;
+    int end;
 public:
     Queue();
     Queue(int max_size);
+    int norm_index(int index);
+    bool is_full();
+    void expand();
     int push(char value);
     int top(char& value, bool do_pop);
     bool is_empty();
@@ -31,18 +28,50 @@ public:
 
 
 Queue::Queue() {
+    values = new int[max_size];
     size = 0;
-    start = nullptr;
-    end = nullptr;
+    start = 0;
+    end = 0;
     this->max_size = -1;
 }
 
 
 Queue::Queue(int max_size) {
+    values = new int[max_size];
     size = 0;
-    start = nullptr;
-    end = nullptr;
+    start = 0;
+    end = 0;
     this->max_size = max_size;
+}
+
+
+int Queue::norm_index(int index) {
+    return (index % max_size + max_size) % max_size;
+}
+
+
+bool Queue::is_full() {
+    return end - start == max_size;
+}
+
+
+void Queue::expand() {
+    start = norm_index(start);
+    end = norm_index(end);
+    int old_capacity = max_size;
+    max_size *= 2;
+    int* tmp_values = realloc(values, max_size * sizeof(int));
+    if (!tmp_values) {
+        return;
+    }
+    values = tmp_values;
+    int i;
+    if (start >= end) {
+        for (i = 0; i < end; i++) {
+            values[i + old_capacity] = values[i];
+        }
+        end += old_capacity;
+    }
 }
 
 
@@ -51,18 +80,12 @@ int Queue::push(char value) {
         std::cout << "Queue is full." << "\n";
         return QUEUE_FULL;
     }
-    Node* new_node = new Node;
-    new_node->value = value;
-    new_node->next = nullptr;
-    size++;
-    if (end == nullptr) {
-        end = new_node;
-        start = new_node;
-        return SUCCESS;
+    if (is_full()) {
+        expand();
     }
-    end->next = new_node;
-    end = new_node;
-    return SUCCESS;
+    values[norm_index(queue, end)] = *value;
+    end++;
+    return 0;
 }
 
 
@@ -130,47 +153,47 @@ int main() {
         switch (command) {
             case 1:
                 std::cin >> value;
-                queue->push(value);
+                push(value);
                 break;
             case 2:
-                if (queue->is_empty()) {
+                if (is_empty()) {
                     std::cout << "Queue is empty" << "\n";
                 } else {
-                    queue->top(value);
+                    top(value);
                     std::cout << "Top value: "  << value << "\n";
                 }
                 break;
             case 3:
-                if (queue->is_empty()) {
+                if (is_empty()) {
                     std::cout << "Queue is empty" << "\n";
                 } else {
-                    queue->top(value, true);
+                    top(value, true);
                     std::cout << "Popped value: " << value << "\n";
                 }
                 break;
             case 4:
-                if (queue->is_empty()) {
+                if (is_empty()) {
                     std::cout << "Queue is empty" << "\n";
                 } else {
                     std::cout << "Queue is not empty" << "\n";
                 }
                 break;
             case 5:
-                std::cout << "Queue size: " << queue->get_size() << "\n";
+                std::cout << "Queue size: " << get_size() << "\n";
                 break;
             case 6:
-                queue->clear();
+                clear();
                 std::cout << "Queue cleared." << "\n";
                 break;
             case 7:
-                queue->clear(true);
+                clear(true);
                 std::cout << "Queue filled with zeros." << "\n";
                 break;
             default:
                 break;
         }
     } while (command != 0);
-    queue->clear();
+    clear();
     delete queue;
     return 0;
 }
